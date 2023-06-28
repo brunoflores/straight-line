6f731ccc6105f816e69db08bad0fea6172437170
Bruno Flores <bruno@brunoflores.com.br>
2023-06-29 03:50:51 +1000

    Wire.

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+module Ast = Straight_line.Ast
+module Parser = Straight_line.Parser_fe
+
+let () =
+  let usage = "Compile" in
+  let spec = [] in
+  let files = ref [] in
+  let readfname s = files := s :: !files in
+  let _ = Arg.parse spec readfname usage in
+  let _ = Parser.parse !files in
+  ()
