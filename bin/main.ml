06cae521c18e2a78b93a4a2d66f06c00a98989cd
Bruno Flores <bruno@brunoflores.com.br>
2023-06-30 07:08:24 +1000

    Drive.

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,8 @@
 module Ast = Straight_line.Ast
 module Parser = Straight_line.Parser_fe
 
+let interpret (prog : Ast.stm list) : unit = failwith ""
+
 let () =
   let usage = "Compile" in
   let spec = [] in
@@ -8,11 +10,8 @@ let () =
   let readfname s = files := s :: !files in
   let _ = Arg.parse spec readfname usage in
   let file = List.hd !files in
-  let _ =
-    match Parser.parse file with
-    | Ok _ -> ()
-    | Error e ->
-        Printf.eprintf "%s\n" e;
-        exit 1
-  in
-  ()
+  match Parser.parse file with
+  | Ok prog -> interpret prog
+  | Error e ->
+      Printf.eprintf "%s\n" e;
+      exit 1
