d594a8870c37c7708b53b572372c1b5bc5946c5d
Bruno Flores <bruno@brunoflores.com.br>
2023-07-01 22:48:54 +1000

    Move.

--- bin/main.ml
+++ bin/main.ml
@@ -2,36 +2,6 @@ module Parser = Straight_line.Parser_fe
 module Interpreter = Straight_line.Interpreter
 module Ast = Straight_line.Ast
 
-let pretty_print_err (p1, p2) =
-  let ( { Ast.pos_fname; pos_lnum; pos_cnum = c1; pos_bol = b1 },
-        { Ast.pos_cnum = c2; pos_bol = b2; _ } ) =
-    (p1, p2)
-  in
-  (* Character offset minus line offset equals column number *)
-  let c1 = c1 - b1 in
-  let c2 = c2 - b2 in
-  let ic = open_in pos_fname in
-  let lnum = ref pos_lnum in
-  let line = ref "" in
-  let caret =
-    String.init c2 (fun i -> if i >= c1 && i <= c2 then '^' else ' ')
-  in
-  try
-    while !lnum > 0 do
-      line := input_line ic;
-      decr lnum
-    done;
-    let margin = Format.sprintf "%d | " pos_lnum in
-    Printf.eprintf "%s%s\n%!" margin !line;
-    let margin = String.make (String.length margin) ' ' in
-    let reset = "\027[0m" in
-    let red = "\027[31m" in
-    Printf.eprintf "%s%s%s%s\n%!" red margin caret reset;
-    close_in ic
-  with e ->
-    close_in_noerr ic;
-    raise e
-
 let () =
   let usage = "Compile" in
   let spec = [] in
@@ -45,10 +15,10 @@ let () =
       with Interpreter.Unbound_identifier pos ->
         let header = "Unbound identifier" in
         Printf.eprintf "%s\n%!" header;
-        pretty_print_err pos;
+        Ast.pretty_print_err pos;
         exit 1)
   | Error (Parser.SyntaxError (header, pos)) ->
       Printf.eprintf "%s\n%!" header;
-      pretty_print_err pos;
+      Ast.pretty_print_err pos;
       exit 1
   | Error Parser.Empty -> exit 1
--- lib/ast.ml
+++ lib/ast.ml
@@ -17,3 +17,33 @@ let pos_of_lexing_position (pos : Lexing.position) : pos =
     pos_bol = pos.pos_bol;
     pos_cnum = pos.pos_cnum;
   }
+
+let pretty_print_err (p1, p2) =
+  let ( { pos_fname; pos_lnum; pos_cnum = c1; pos_bol = b1 },
+        { pos_cnum = c2; pos_bol = b2; _ } ) =
+    (p1, p2)
+  in
+  (* Character offset minus line offset equals column number *)
+  let c1 = c1 - b1 in
+  let c2 = c2 - b2 in
+  let ic = open_in pos_fname in
+  let lnum = ref pos_lnum in
+  let line = ref "" in
+  let caret =
+    String.init c2 (fun i -> if i >= c1 && i <= c2 then '^' else ' ')
+  in
+  try
+    while !lnum > 0 do
+      line := input_line ic;
+      decr lnum
+    done;
+    let margin = Format.sprintf "%d | " pos_lnum in
+    Printf.eprintf "%s%s\n%!" margin !line;
+    let margin = String.make (String.length margin) ' ' in
+    let reset = "\027[0m" in
+    let red = "\027[31m" in
+    Printf.eprintf "%s%s%s%s\n%!" red margin caret reset;
+    close_in ic
+  with e ->
+    close_in_noerr ic;
+    raise e
