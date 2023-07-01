943bf49413d21b03549522f6142c73eb2b62097b
Bruno Flores <bruno@brunoflores.com.br>
2023-07-02 00:03:25 +1000

    Rename.

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -2,10 +2,10 @@ exception Unbound_identifier of (Ast.pos * Ast.pos)
 
 type env = (Ast.id * int) list
 
-let rec interpretStm (env : env) (stm : Ast.stm) : env =
+let rec interpret_stm (env : env) (stm : Ast.stm) : env =
   match stm with
   | Ast.AssignStm (id, exp) ->
-      let exp, env = interpretExp env exp in
+      let exp, env = interpret_exp env exp in
       (* We add this new binding to the environment list trusting that
          [List.assoc] returns the first binding found when searching. *)
       (id, exp) :: env
@@ -18,7 +18,7 @@ let rec interpretStm (env : env) (stm : Ast.stm) : env =
       (* [print] takes a list of expressions to print, so we evaluate them from
          left to right, augmenting the environment and passing it to the next *)
       let interpret_and_augment (results, env) e =
-        let e', env' = interpretExp env e in
+        let e', env' = interpret_exp env e in
         (e' :: results, env')
       in
       (* We fold left and then reverse the resulting list *)
@@ -27,19 +27,19 @@ let rec interpretStm (env : env) (stm : Ast.stm) : env =
       (* We return the environment that resulted from the last expression *)
       env'
 
-and interpretExp (env : env) (exp : Ast.exp) : int * env =
+and interpret_exp (env : env) (exp : Ast.exp) : int * env =
   match exp with
   | Ast.NumExp e -> (e, env)
   | Ast.EffectfulExp (s, e) ->
-      let env' = interpretStm env s in
-      interpretExp env' e
+      let env' = interpret_stm env s in
+      interpret_exp env' e
   | Ast.IdExp (id, pos) -> (
       match List.assoc_opt id env with
       | Some e -> (e, env)
       | None -> raise @@ Unbound_identifier pos)
   | Ast.OpExp (e1, op, e2) ->
-      let e1, env' = interpretExp env e1 in
-      let e2, env'' = interpretExp env' e2 in
+      let e1, env' = interpret_exp env e1 in
+      let e2, env'' = interpret_exp env' e2 in
       let meta_op =
         match op with
         | Ast.Plus -> ( + )
@@ -53,5 +53,5 @@ and interpretExp (env : env) (exp : Ast.exp) : int * env =
 let interpret (prog : Ast.stm list) : unit =
   let emptyenv : env = [] in
   (* We discard the last environment *)
-  let _ = List.fold_left interpretStm emptyenv prog in
+  let _ = List.fold_left interpret_stm emptyenv prog in
   ()
